8dfb8ff3fbba6646d024145afa2dfa442c2740ac
Nishi <nishi@nishi.boats>
2024-05-18 02:37:25 +0000

    format update
    git-svn-id: file:///raid/svn-main/nishi-dataworks/trunk@20 d4a5a174-5a4a-5b4b-b672-37683c10d7d5

--- Library/dataworks.c
+++ Library/dataworks.c
@@ -46,6 +46,6 @@ const char* dataworks_get_version(void) { return dataworks_version; }
 const char* dataworks_get_compile_date(void) { return dataworks_compile_date; }
 const char* dataworks_get_platform(void) { return dataworks_platform; }
 char dataworks_get_endian(void) {
-	unsigned short n = 1;
+	volatile unsigned short n = 1;
 	return *(char*)&n == 1 ? 'L' : 'B';
 }
