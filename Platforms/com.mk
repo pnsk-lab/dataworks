346d59eb1efd8596a57a8bb24f60813cba7b8fee
Nishi <nishi@nishi.boats>
2024-05-21 18:46:53 +0000

    i hope this works
    git-svn-id: file:///raid/svn-main/nishi-dataworks/trunk@57 d4a5a174-5a4a-5b4b-b672-37683c10d7d5

--- Platforms/com.mk
+++ Platforms/com.mk
@@ -3,3 +3,5 @@
 WATCOM_PLATFORM=com
 include Platforms/watcom.mk
 EXEC_SUFFIX=.com
+CFLAGS += -march=i86
+LDFLAGS += -march=i86
--- Platforms/dos.mk
+++ Platforms/dos.mk
@@ -3,3 +3,5 @@
 WATCOM_PLATFORM=dos
 include Platforms/watcom.mk
 EXEC_SUFFIX=.exe
+CFLAGS += -march=i86
+LDFLAGS += -march=i86
