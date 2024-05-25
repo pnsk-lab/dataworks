1c5ae5f8f0c5627573498316cd86cd2ca0c2c264
Nishi <nishi@nishi.boats>
2024-05-25 13:36:51 +0000

    add dosbox target to watcom target
    git-svn-id: file:///raid/svn-main/nishi-dataworks/trunk@103 d4a5a174-5a4a-5b4b-b672-37683c10d7d5

--- Platforms/watcom.mk
+++ Platforms/watcom.mk
@@ -14,3 +14,21 @@ PLATFORM_M = $(WATCOM_PLATFORM)
 PLATFORM_P = $(WATCOM_PLATFORM)
 CFLAGS += -DDOS -b $(WATCOM_PLATFORM)
 LDFLAGS += -b $(WATCOM_PLATFORM)
+
+dosbox: no-doc
+	echo 'create_table("test", "string:key", "double:value");' > op.txt
+	echo '.tables' >> op.txt
+	echo "[cpu]" > dosbox.conf
+	echo "cycles=12000" >> dosbox.conf
+	echo "[autoexec]" >> dosbox.conf
+	echo "mount c: ." >> dosbox.conf
+	echo "c:" >> dosbox.conf
+	echo "copy Client\*$(EXEC_SUFFIX) dw$(EXEC_SUFFIX)" >> dosbox.conf
+	echo "dw -NC -f op.txt --create db.dwf" >> dosbox.conf
+	echo "pause" >> dosbox.conf
+	echo "del db.dwf" >> dosbox.conf
+	echo "del dw.exe" >> dosbox.conf
+	echo "exit" >> dosbox.conf
+	if [ ! -e "dos4gw.exe" ]; then wget "https://github.com/yetmorecode/dos32a-ng/releases/download/9.1.2/DOS32ANG.EXE" -O dos4gw.exe ; fi
+	dosbox
+	rm op.txt
