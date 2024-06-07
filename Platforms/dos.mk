aab6c74a2d5ca74c6a348c7160dde3daf65d1a93
Nishi <nishi@nishi.boats>
2024-06-07 06:17:04 +0000

    have 8kb stack
    git-svn-id: file:///raid/svn-main/nishi-dataworks/trunk@169 d4a5a174-5a4a-5b4b-b672-37683c10d7d5

--- Platforms/dos.mk
+++ Platforms/dos.mk
@@ -3,7 +3,7 @@
 WATCOM_PLATFORM=dos
 include Platforms/watcom.mk
 EXEC_SUFFIX=.exe
-STACK_SIZE = 2048
+STACK_SIZE = 8192
 CFLAGS += -march=i86 -mstack-size=$(STACK_SIZE)
 LDFLAGS += -march=i86 -mstack-size=$(STACK_SIZE)
 SERVER_MODULE = hayes.o
