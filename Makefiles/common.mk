1b1bbbc28d3897000351717dec916106bbb9bd75
Nishi <nishi@nishi.boats>
2024-06-19 01:35:30 +0000

    add makefiles
    git-svn-id: file:///raid/svn-main/nishi-dataworks/trunk@360 d4a5a174-5a4a-5b4b-b672-37683c10d7d5

--- Makefiles/common.mk
+++ Makefiles/common.mk
@@ -63,47 +63,6 @@ clean:
 	$(MAKE) -C ./Document clean $(COMPILE_FLAGS)
 	$(MAKE) -C ./Tool clean $(COMPILE_FLAGS)
 
-dos-installer:
-	if [ ! "$(FORMAT)" = "NO" ]; then $(MAKE) PLATFORM=dos clean ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then $(MAKE) PLATFORM=dos no-doc ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then rm -f install.img ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then mformat -C -f $(FLOPPY_SIZE) -v DWINST -i install.img :: ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then mcopy -i install.img Client/*.exe ::dw.exe ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then mcopy -i install.img Server/*.exe ::dwserv.exe ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then mcopy -i install.img RemoteClient/*.exe ::dwrcli.exe ; fi
-	$(MAKE) PLATFORM=dos TARGET=clean ./Installer
-	$(MAKE) PLATFORM=dos ./Installer
-	mcopy -oi install.img Installer/*.exe ::install.exe
-	$(MAKE) PLATFORM=dos TARGET=clean ./Installer
-	$(MAKE) PLATFORM=dos INDEP=jp INDEP_TO=cp932 ./Installer
-	$(MAKE) ./Tool
-	$(MAKE) -C ./Document README.DOC
-	mcopy -toi install.img Document/README.DOC ::
-	mcopy -oi install.img Installer/*.exe ::instajp.exe
-	if [ ! -e lha.exe ]; then wget http://f.nishi.boats/f/g/lha.exe ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then mcopy -i install.img lha.exe ::lha.exe ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then mkdir -p install-workdir ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "[cpu]" > install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "cycles=30000" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "[autoexec]" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "imgmount -t floppy a: ./install.img" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "a:" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "mount c: ./install-workdir" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "lha a C:\EXTRACT.LZH dw.exe dwserv.exe dwrcli.exe readme.doc" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "del dw.exe" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "del dwserv.exe" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "del dwrcli.exe" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "c:" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "a:\lha s extract.lzh" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "a:" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "copy C:\EXTRACT.EXE" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "del lha.exe" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then echo "exit" >> install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then dosbox -conf install-dosbox.conf ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then rm -rf install-workdir ; fi
-	if [ ! "$(FORMAT)" = "NO" ]; then rm -f install-dosbox.conf ; fi
-	$(MAKE) clean
-
 get-version:
 	@echo $(VERSION)
 
@@ -111,3 +70,4 @@ include Makefiles/archive.mk
 include Makefiles/dosbox.mk
 include Makefiles/package.mk
 include Makefiles/test.mk
+include Makefiles/installer.mk
