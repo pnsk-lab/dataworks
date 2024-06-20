eb42d0b189973ef97f727d254983407b339f6c96
Nishi <nishi@nishi.boats>
2024-06-20 09:02:56 +0000

    fix os/2 build
    git-svn-id: file:///raid/svn-main/nishi-dataworks/trunk@401 d4a5a174-5a4a-5b4b-b672-37683c10d7d5

--- Makefiles/Platforms/os2.mk
+++ Makefiles/Platforms/os2.mk
@@ -3,7 +3,10 @@
 WATCOM_PLATFORM=os2
 include Makefiles/Platforms/watcom.mk
 EXEC_SUFFIX=.exe
+STACK_SIZE = 8192
 RCLI=
 SERVER=
+CFLAGS += -march=i86 -mstack-size=$(STACK_SIZE)
+LDFLAGS += -march=i86 -mstack-size=$(STACK_SIZE)
 SERVER_MODULE = hayes.o
 RCLI_MODULE = hayes.o
